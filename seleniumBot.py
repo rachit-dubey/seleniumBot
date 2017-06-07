95c985aeaaa84d2fe86c732c62a037b41f576c31
Rachit Dubey <rachitduncan@gmail.com>
2017-06-07 15:52:24 -0700

    Update seleniumBot.py

--- seleniumBot.py
+++ seleniumBot.py
@@ -16,17 +16,23 @@ driver.set_window_size(360, 480) #resize the window here before starting the gam
 driver.get("https://dry-anchorage-61733.herokuapp.com/")
 
 actions = ActionChains(driver)
+#predefine all the actions here
+action1 = actions.key_down(Keys.ARROW_UP).send_keys("n").key_up(Keys.ARROW_UP)
+action2 = actions.key_down(Keys.ARROW_DOWN).send_keys("n").key_up(Keys.ARROW_DOWN)
+action3 = actions.key_down(Keys.ARROW_LEFT).send_keys("n").key_up(Keys.ARROW_LEFT)
+action4 = actions.key_down(Keys.ARROW_RIGHT).send_keys("n").key_up(Keys.ARROW_RIGHT)
+
 element = driver.find_element_by_tag_name("canvas")
 time.sleep(1)
 actions.click(element).perform() #really need to first click on the canvas to do anything
 #to time the while loop
-t_end = time.time() + 10
-
+t_end = time.time() + 40
 count = 0
 while time.time() < t_end:	
 	act = random.randint(1,4)
-	im = output(act,actions,driver)	
+	output(act,driver,action1,action2,action3,action4,count)	
 	count = count+1
 	
 driver.close()
+	
 	
